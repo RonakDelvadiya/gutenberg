4dea489da7794a0754f9693c0cafc66557306d3e
RonakDelvadiya <ronakdelvadiya01@gmail.com>
2024-10-18 16:17:27 +0530

    [added][collect static files]

--- gutenberg/settings.py
+++ gutenberg/settings.py
@@ -138,3 +138,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 25
 }
+
+STATIC_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+print("STATIC_DIRS",STATIC_DIRS)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
